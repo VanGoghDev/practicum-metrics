2495bb55c08c56833f97bccdda2875f25438680a
Firsov Kirill <kifirsov@rttv.ru>
2024-04-26 19:58:38 +0300

    test commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	// config
+
+	// logger
+
+	// storage
+
+	// router
+	mux := setupMux()
+	mux.HandleFunc(`/`, testHandler)
+	mux.HandleFunc(`/update/`, updateHandler)
+
+	http.ListenAndServe(`:8333`, mux)
+}
+
+func testHandler(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Hello"))
+}
+
+func updateHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// p := strings.Split(req.URL.Path, "/")
+
+}
+
+func setupMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	return mux
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VanGoghDev/practicum-metrics
+
+go 1.21.9
