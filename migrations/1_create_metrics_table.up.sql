1e4c7c8b8597bfe68ff6d383f20042368a15905f
Cyril Firsov <kifirigor@gmail.com>
2024-06-26 21:54:57 +0300

    удалил ненужные миграции

--- migrations/1_create_metrics_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS metrics(
-			name 	VARCHAR(200) PRIMARY KEY,
-			g_type 	VARCHAR(200) NOT NULL,
-			g_value DOUBLE PRECISION NOT NULL,
-			delta 	bigint NOT NULL,
-			UNIQUE(name, g_type)
-		)
