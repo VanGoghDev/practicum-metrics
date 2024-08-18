1fac800d6fb4e38b43e6a02c336ae04cd98850ce
Cyril Firsov <kifirigor@gmail.com>
2024-08-18 21:09:38 +0300

    warn fix

--- internal/storage/memstorage/memstorage_test.go
+++ internal/storage/memstorage/memstorage_test.go
@@ -315,16 +315,13 @@ func BenchmarkSaveGauge(b *testing.B) {
 	}
 }
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func randSeq(n int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	b := make([]rune, n)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[r.Intn(len(letters))]
 	}
 	return string(b)
 }
